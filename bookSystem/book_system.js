b67bde6b17c8e2e9911d308a3a972798b4bdbc8b
zayneb <dhibzayneb7@gmail.com>
2025-07-20 14:41:43 -0400

    mise a jour du dossier books

---
+++ bookSystem/book_system.js
@@ -0,0 +1,47 @@
+let books=[];
+function addBook(){
+    const bookName=document.getElementById('bookName').value;
+    const authorName=document.getElementById('authorName').value;
+    const bookDescription=document.getElementById('bookDescription').value;
+     const pagesNumber=parseInt(document.getElementById('pagesNumber').value);
+     if(bookName && authorName && bookDescription && !isNaN(pagesNumber)){
+        const book={
+            name:bookName,
+            authorName:authorName,
+            bookDescription:bookDescription,
+            pagesNumber:pagesNumber
+        };
+        books.push(book);
+        showbooks();
+        clearInputs();
+     }else{
+        alert("Please fill in all fields correctly");
+     }
+
+}
+function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+        <p><strong>Book Name: </strong>${book.name}</p>
+        <p><strong>Author Name:</strong> ${book.authorName}</p>
+        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="editbook(${index})">Edit</button>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+
+}
+function editbook(index) {
+    const book = books[index];
+    document.getElementById('bookName').value = book.name;
+    document.getElementById('authorName').value = book.authorName;
+    document.getElementById('bookDescription').value = book.bookDescription;
+    document.getElementById('pagesNumber').value = book.pagesNumber;
+    books.splice(index, 1); // Remove old entry
+    showbooks(); // Refresh list
+  }
+  function clearInputs() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
